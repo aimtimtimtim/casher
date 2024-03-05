5e1f8933717343151ea175106e3095b9636b0d5e
Tim <agayevdev@gmail.com>
2024-03-05 22:59:06 +0100

    news

--- src/pages/MainScreen/MainScreen.jsx
+++ src/pages/MainScreen/MainScreen.jsx
@@ -2,6 +2,7 @@ import AppBar from "src/components/AppBar/AppBar.jsx";
 import History from "src/components/History/History.jsx";
 import Add from "src/components/Icons/Add.jsx";
 import Edit from "src/components/Icons/Edit.jsx";
+import NewExprense from "src/components/Icons/NewExprense.jsx";
 import Logo from "src/components/Logo/Logo.jsx";
 import UserBudget from "src/components/UserBudget/UserBudget.jsx";
 import styles from './styles.module.scss'
@@ -16,6 +17,7 @@ const MainScreen = () => {
 			
 			<UserBudget/>
 			<History/>
+			<div className={styles.newExpense}><NewExprense/></div>
 		<AppBar/>
 		</div>
 	);
